d6f2adec12b06a76411d8ac75c0d7380ab1e988f
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-02-14 22:05:28 -0300

    comandos no alert

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+let nome = prompt('Digite seu nome')
+alert('Bem-vindo ' + nome + ', vamos somar 2 números')
+
+let valor1 = parseInt(prompt('Digite o primeiro número'))
+let valor2 = parseInt(prompt('Digite o segundo número'))
+
+let valor = valor1 + valor2
+
+alert('O valor da soma é = ' + valor)
+
+console.log(parseInt(valor1) + parseInt(valor2))
